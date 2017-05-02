abfd45eb02a4b95750b4524404b381e83965cb09
paladox <paladox@users.noreply.github.com>
2017-05-02 22:56:07 +0100

    Update plugins.pp

--- manifests/plugins.pp
+++ manifests/plugins.pp
@@ -23,12 +23,6 @@ class icinga2::plugins {
         group  => 'root',
         mode   => '0755',
     }
-    file { '/usr/lib/nagios/plugins/eventhandlers/submit_check_result':
-        source => 'puppet:///modules/icinga2/submit_check_result.sh',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
     file { '/var/run/icinga2/cmd':
         ensure => directory,
         owner  => 'nagios',
@@ -50,47 +44,5 @@ class icinga2::plugins {
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
-    }
-
-    File <| tag == nagiosplugin |>
-
-    file { '/usr/lib/nagios/plugins/check_mysql-replication.pl':
-        source => 'puppet:///modules/icinga2/check_mysql-replication.pl',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-    file { '/usr/lib/nagios/plugins/check_longqueries':
-        source => 'puppet:///modules/icinga2/check_longqueries.pl',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-    file { '/usr/lib/nagios/plugins/check_MySQL.php':
-        source => 'puppet:///modules/icinga2/check_MySQL.php',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-    file { '/usr/lib/nagios/plugins/check_ram.sh':
-        source => 'puppet:///modules/icinga2/check_ram.sh',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    class { '::nagios_common::commands':
-    }
-
-    include ::passwords::nagios::mysql
-
-    $nagios_mysql_check_pass = $passwords::nagios::mysql::mysql_check_pass
-
-    nagios_common::check_command::config { 'mysql.cfg':
-        ensure     => present,
-        content    => template('icinga2/check_commands/mysql.cfg.erb'),
-        config_dir => '/etc/icinga2',
-        owner      => 'root',
-        group      => 'root',
     }
 }
