56e255edf32a1f22ef7d49375fbcf4045fd715d1
paladox <paladox@users.noreply.github.com>
2017-05-03 01:17:25 +0100

    Update web.pp

--- manifests/web.pp
+++ manifests/web.pp
@@ -2,7 +2,20 @@
 #
 # Sets up an apache instance for icinga web interface,
 # protected with ldap authentication
-class icinga2::web {
+class icinga2::web(
+    $icingaweb_db_host = hiera('icingaweb_db_host'),
+    $icingaweb_db_name = hiera('icingaweb_db_name'),
+    $icingaweb_user_name = hiera('icingaweb_user_name'),
+    $icingaweb_password = hiera('icingaweb_password'),
+    $icinga_ido_db_host = hiera('icinga_ido_db_host'),
+    $icinga_ido_db_name = hiera('icinga_ido_db_name'),
+    $icinga_ido_user_name = hiera('icinga_ido_user_name'),
+    $icinga_ido_password = hiera('icinga_ido_password'),
+    $director_db_host = hiera('director_db_host'),
+    $director_db_name = hiera('director_db_name'),
+    $director_user_name = hiera('director_user_name'),
+    $director_password = hiera('director_password')
+) {
     include ::icinga2
 
     apt::repository { 'icingaweb2':
@@ -39,30 +52,6 @@ class icinga2::web {
     require_package('php5')
     require_package('php5-dev')
     require_package('php5-gd')
-    
-    $icingaweb_db_host = hiera('icingaweb_db_host')
-
-    $icingaweb_db_name = hiera('icingaweb_db_name')
-
-    $icingaweb_user_name = hiera('icingaweb_user_name')
-
-    $icingaweb_password = hiera('icingaweb_password')
-
-    $icinga_ido_db_host = hiera('icinga_ido_db_host')
-
-    $icinga_ido_db_name = hiera('icinga_ido_db_name')
-
-    $icinga_ido_user_name = hiera('icinga_ido_user_name')
-
-    $icinga_ido_password = hiera('icinga_ido_password')
-
-    $director_db_host = hiera('director_db_host')
-
-    $director_db_name = hiera('director_db_name')
-
-    $director_user_name = hiera('director_user_name')
-
-    $director_password = hiera('director_password')
 
     file { '/etc/icingaweb2':
         ensure => 'directory',
